fdea9068d84414ac6bcc25d80744aff11247da3b
aufardinata <88268107+aufardinata@users.noreply.github.com>
2021-08-02 12:11:56 +0700

    Update Token.sol

--- Token.sol
+++ Token.sol
@@ -16,7 +16,7 @@ contract Token is TRC20, TRC20Detailed {
     /**
      * @dev Constructor that gives msg.sender all of existing tokens.
      */
-    constructor () public TRC20Detailed("Machining", "MAC", 10) {
+    constructor () public TRC20Detailed("ujangdirman", "ujg", 10) {
         _mint(msg.sender, 10000 * (10 ** uint256(decimals())));
     }
 }
